3312e8bbb5fd2c7c06a8b15078df929e49b365ae
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-12-26 20:48:32 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Welcome to {{title}}!</h1>
+
 
     <router-outlet></router-outlet>
   `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-front-end';
+
 }
